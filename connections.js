ed1b0ab5a0f8466645f694099b00e9b284039152
semperry <ryan.curtis88@gmail.com>
2019-07-31 12:02:43 -0600

    added redis routes

--- connections.js
+++ connections.js
@@ -3,7 +3,7 @@ const redis = require("redis");
 module.exports = {
   createConnection: () => {
     return new Promise((resolve, reject) => {
-      const client = redis.createClient(process.env.REDIS_URL);
+      const client = redis.createClient(process.env.REDIS_URL || null);
 
       client.on("connect", () => {
         resolve(client);
--- routes/redis-routes.js
+++ routes/redis-routes.js
@@ -7,6 +7,51 @@ const redis = require("redis");
 const connect = require("../connections");
 
 // POST to create completion flag
+RedisRouter.route("/complete").post((req, res) => {
+  connect.createConnection().then(client => {
+    const id = req.body.id;
+    const complete = true;
+
+    client.hset(id, "complete", complete, redis.print);
+
+    client.hgetall(id, (err, results) => {
+      if (results) {
+        res.send(results);
+      } else {
+        res.send(err);
+      }
+    });
+
+    client.expire(id, 60);
+
+    client.quit((err, reply) => {
+      if (!err) {
+        console.log(reply);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
 // GET to check if flag exists
+RedisRouter.route("/completed/:id").get((req, res) => {
+  connect.createConnection().then(client => {
+    client.hgetall(req.params.id, (err, results) => {
+      if (results) {
+        res.status(200).send("Still complete");
+      } else {
+        res.status(404).send("Expired");
+      }
+    });
+    client.quit((err, reply) => {
+      if (!err) {
+        console.log("Get quit response" + reply);
+      } else {
+        console.log("Get quit error" + err);
+      }
+    });
+  });
+});
 
 module.exports = RedisRouter;
